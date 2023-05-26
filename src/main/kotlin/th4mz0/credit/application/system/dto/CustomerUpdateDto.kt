f064fbe11643321463b6fe341648a5a1f66da24e
Jairo L Alves <jairoauves8@gmail.com>
2023-05-26 12:26:12 -0300

    adicionando tratamento de excecao

--- src/main/kotlin/th4mz0/credit/application/system/dto/CustomerUpdateDto.kt
+++ src/main/kotlin/th4mz0/credit/application/system/dto/CustomerUpdateDto.kt
@@ -1,13 +1,23 @@
 package th4mz0.credit.application.system.dto
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import th4mz0.credit.application.system.entity.Customer
 import java.math.BigDecimal
 
 data class CustomerUpdateDto(
+    @field:NotEmpty(message = "Invalid input")
     val firstName: String,
+
+    @field:NotEmpty(message = "Invalid input")
     val lastName: String,
+
+    @field:NotNull(message = "Invalid input")
     val income: BigDecimal,
+
+    @field:NotEmpty(message = "Invalid input")
     val zipCode: String,
+    @field:NotEmpty(message = "Invalid input")
     val street: String
 
 ) {
--- src/main/kotlin/th4mz0/credit/application/system/service/impl/CustomerService.kt
+++ src/main/kotlin/th4mz0/credit/application/system/service/impl/CustomerService.kt
@@ -2,6 +2,7 @@ package th4mz0.credit.application.system.service.impl
 
 import org.springframework.stereotype.Service
 import th4mz0.credit.application.system.entity.Customer
+import th4mz0.credit.application.system.exception.BusinessException
 import th4mz0.credit.application.system.repository.CustomerRepository
 import th4mz0.credit.application.system.service.ICustomerService
 
@@ -16,12 +17,14 @@ class CustomerService(
 
     override fun findById(id: Long): Customer {
          return this.customerRepository.findById(id).orElseThrow{
-             throw RuntimeException("Id $id not found")
+             throw BusinessException("Id $id not found")
 
         }
     }
 
     override fun delete(id: Long) {
-        return this.customerRepository.deleteById(id)
+        val customer: Customer = this.findById(id)
+        this.customerRepository.delete(customer)
+
     }
 }
